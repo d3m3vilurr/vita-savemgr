16d6ffe484b930f520a70fb3cf6c85ef688ec531
Sunguk Lee <d3m3vilurr@gmail.com>
2016-11-04 03:49:23 +0900

    Update build toolchain
    Replace base system to frangarcj's prebuilt binaries
    Additional lib still use ooPo's scripts

--- .travis.d/install_toolchain.sh
+++ .travis.d/install_toolchain.sh
@@ -3,50 +3,21 @@ mkdir -p $VITASDK/tmp
 mkdir -p $HOME/opt
 cd $VITASDK/tmp
 
-if [ ! -f $HOME/opt/lib/libjansson.so ]; then
-    git clone --depth=50 https://github.com/akheron/jansson.git
-
-    mkdir -p jansson/build
-    cd jansson/build
-
-    cmake -DCMAKE_INSTALL_PREFIX=$HOME/opt -DJANSSON_BUILD_SHARED_LIBS=ON -DJANSSON_BUILD_DOCS=OFF .. > /dev/null
-    cmake --build . > /dev/null
-    make install > /dev/null || exit 1
-
-    cd ../..
-    rm -rf jansson
-fi
-
-if [ ! -f $HOME/opt/lib/libzip.so ]; then
-    git clone --depth=50 https://github.com/nih-at/libzip.git
-
-    mkdir -p libzip/build
-    cd libzip/build
-
-    cmake -DCMAKE_INSTALL_PREFIX=$HOME/opt .. > /dev/null
-    cmake --build . > /dev/null
-    make all install > /dev/null
-
-    cd ../..
-    rm -rf libzip
-fi
-
 if [ ! -f $VITASDK/bin/arm-vita-eabi-gcc ]; then
-    git clone https://github.com/ooPo/vitatoolchain.git
+    mkdir vitatoolchain
+    cd vitatoolchain
+    TOOLCHAIN=$(curl -s https://api.github.com/repos/frangarcj/buildscripts/releases | grep "browser_download_url" | grep "linux" | head -n 1 | awk '{print $2}' | sed -r 's/"//g')
+    curl -s -L -o vitatoolchain.tar.bz2 ${TOOLCHAIN}
+    tar xjf vitatoolchain.tar.bz2
+    mv vitasdk/* $VITASDK/
 
-    mkdir -p vitatoolchain/build
-    cd vitatoolchain/build
+    curl https://raw.githubusercontent.com/ooPo/vitatoolchain/master/scripts/007-vdpm.sh | bash
+    curl https://raw.githubusercontent.com/ooPo/vitatoolchain/master/scripts/008-sdl-vita.sh | bash
+    curl https://raw.githubusercontent.com/ooPo/vitatoolchain/master/scripts/009-libftpvita.sh | bash
 
-    bash ../scripts/001-binutils.sh > /dev/null || exit 1
-    bash ../scripts/002-libelf.sh > /dev/null || exit 1
-    bash ../scripts/003-vita-toolchain.sh > /dev/null || exit 1
-    bash ../scripts/004-vita-headers.sh > /dev/null || exit 1
-    bash ../scripts/005-gcc.sh > /dev/null || exit 1
-    #bash ../scripts/006-gdb.sh > /dev/null || exit 1
-    bash ../scripts/007-vdpm.sh > /dev/null || exit 1
-    bash ../scripts/008-sdl-vita.sh > /dev/null || exit 1
-    bash ../scripts/009-libftpvita.sh > /dev/null || exit 1
-
-    cd ../..
-    rm -rf vitatoolchain/build
+    cd ..
+    rm -rf vitatoolchain
 fi
+
+cd ..
+rm -rf $VITASDK/tmp
