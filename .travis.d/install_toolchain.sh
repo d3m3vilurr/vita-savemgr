8fe39895de7934f5aadfac79b47ea841fd1f9ce0
Sunguk Lee <d3m3vilurr@gmail.com>
2019-05-20 00:46:46 +0900

    travis: Fix build errors
    - update download toolchain script to detect archive
    - bump up travis to xenial
    - split cmake rule to detect weak lib

--- .travis.d/install_toolchain.sh
+++ .travis.d/install_toolchain.sh
@@ -3,10 +3,14 @@ mkdir -p $VITASDK/tmp
 mkdir -p $HOME/opt
 cd $VITASDK/tmp
 
+get_download_link () {
+  curl -sL https://github.com/vitasdk/vita-headers/raw/master/.travis.d/last_built_toolchain.py | python - $@
+}
+
 if [ ! -f $VITASDK/bin/arm-vita-eabi-gcc ]; then
     mkdir vitatoolchain
     cd vitatoolchain
-    curl -s https://api.github.com/repos/vitasdk/autobuilds/releases | grep browser_download_url | grep linux | head -n 1 | cut -d '"' -f 4 | xargs curl -L | tar xj
+    curl -L $(get_download_link master linux) | tar xj
     mv vitasdk/* $VITASDK/
 
     curl https://raw.githubusercontent.com/ooPo/vitatoolchain/master/scripts/007-vdpm.sh | bash
